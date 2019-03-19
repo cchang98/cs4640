aec7ccbe64b2f028ce19fc7d0effc8df089ce44c
cchang98 <32622779+cchang98@users.noreply.github.com>
2019-03-18 22:01:23 -0400

    Add files via upload
    add <script src="tophikes.js"></script> to the top of the html file before <head>

---
+++ tophikes.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 20) {
+    document.getElementById("btn").style.display = "block";
+  } else {
+    document.getElementById("btn").style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
