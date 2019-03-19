c4a6a396f80206332ce45fcbed799ca0861ac909
dgp3sy <48068870+dgp3sy@users.noreply.github.com>
2019-03-18 22:58:27 -0400

    Add files via upload

--- tophikes.js
+++ tophikes.js
@@ -13,3 +13,38 @@ function topFunction() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
 }
+
+
+document.addEventListener('DOMContentLoaded', init, false);
+function init(){
+  function incrementRag () {
+    var rcount = document.getElementById("rag-count");
+    var newrCount = parseInt(rcount.textContent) + 1;
+    rcount.textContent = newrCount;
+    document.getElementById("rag-button").disabled = true;
+
+  }
+
+  function incrementCrab () {
+    var ccount = document.getElementById("crab-count");
+    var newcCount = parseInt(ccount.textContent) + 1;
+    ccount.textContent = newcCount;
+    document.getElementById("crab-button").disabled = true;
+
+  }
+
+  function incrementKnob () {
+    var kcount = document.getElementById("knob-count");
+    var newkCount = parseInt(kcount.textContent) + 1;
+    kcount.textContent = newkCount;
+    document.getElementById("knob-button").disabled = true;
+
+  }
+  var rbutton = document.getElementById('rag-button');
+  rbutton.addEventListener('click', incrementRag, true);
+  var cbutton = document.getElementById('crab-button');
+  cbutton.addEventListener('click', incrementCrab, true);
+  var kbutton = document.getElementById('knob-button');
+  kbutton.addEventListener('click', incrementKnob, true);
+};
+
